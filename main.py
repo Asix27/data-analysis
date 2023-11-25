62038de9a3c94c94c8546f22778bc9071c713ced
Asix27 <asix2727@gmail.com>
2023-11-25 10:14:30 +0600

    top5

--- main.py
+++ main.py
@@ -1,1 +1,65 @@
-import psycopg2
+## Import the packages
+import numpy as np
+from scipy import stats
+
+
+## Define 2 random distributions
+#Sample Size
+N = 10
+#Gaussian distributed data with mean = 2 and var = 1
+a = np.random.randn(N) + 2
+#Gaussian distributed data with with mean = 0 and var = 1
+b = np.random.randn(N)
+
+
+## Calculate the Standard Deviation
+#Calculate the variance to get the standard deviation
+
+#For unbiased max likelihood estimate we have to divide the var by N-1, and therefore the parameter ddof = 1
+var_a = a.var(ddof=1)
+var_b = b.var(ddof=1)
+
+#std deviation
+s = np.sqrt((var_a + var_b)/2)
+s
+
+
+
+## Calculate the t-statistics
+t = (a.mean() - b.mean())/(s*np.sqrt(2/N))
+
+
+
+## Compare with the critical t-value
+#Degrees of freedom
+df = 2*N - 2
+
+#p-value after comparison with the t 
+p = 1 - stats.t.cdf(t,df=df)
+
+
+print("t = " + str(t))
+print("p = " + str(2*p))
+
+### You can see that after comparing the t statistic with 
+### the critical t value (computed internally) we get a good p value of 0.0005 
+### and thus we reject the null hypothesis and thus it proves that the mean of 
+### the two distributions are different and statistically significant.
+
+
+## Cross Checking with the internal scipy function
+t2, p2 = stats.ttest_ind(a,b)
+print("t = " + str(t2))
+print("p = " + str(p2))
+
+
+Nx = 70
+Ny = 40
+x = np.random.randn(Nx) 
+y = np.random.randn(Ny)+2
+print("Nx: {}, Ny: {}".format(len(x), len(y)))
+t3, p3 = stats.ttest_ind(y,x)
+print("t = " + str(t3))
+print("p = " + str(p3))
+
+from scipy import stats
